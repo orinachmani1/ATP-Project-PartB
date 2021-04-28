41b2ef5e07337bc13306782f5fe8efbbd979ff95
orinachmani1 <73288898+orinachmani1@users.noreply.github.com>
2021-04-28 12:22:17 +0300

    Adding the classes for the maze Server-Client part

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,6 +1,33 @@
 package Client;
 
-public class Client {
-//dsf
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
 
+public class Client implements IClientStrategy {
+
+    @Override
+    public void clientStrategy(InputStream inFromServer, OutputStream outToServer) {
+
+    }
+    private InetAddress serverIP;
+    private int serverPort;
+    private IClientStrategy strategy;
+
+    public Client(InetAddress serverIP, int serverPort, IClientStrategy strategy) {
+        this.serverIP = serverIP;
+        this.serverPort = serverPort;
+        this.strategy = strategy;
+    }
+
+    public void start(){
+        try(Socket serverSocket = new Socket(serverIP, serverPort)){
+            System.out.println("connected to server - IP = " + serverIP + ", Port = " + serverPort);
+            strategy.clientStrategy(serverSocket.getInputStream(), serverSocket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/Server/Server.java
@@ -0,0 +1,73 @@
+package Server;
+
+//import Server.Strategy.IServerStrategy;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+
+public class Server implements IServerStrategy {
+    private int port;
+    private int listeningIntervalMS;
+    private IServerStrategy strategy;
+    private volatile boolean stop;
+   //private final Logger LOG = LogManager.getLogger(); //Log4j2
+
+
+    public Server(int port, int listeningIntervalMS, IServerStrategy strategy) {
+        this.port = port;
+        this.listeningIntervalMS = listeningIntervalMS;
+        this.strategy = strategy;
+    }
+
+    public void start(){
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            serverSocket.setSoTimeout(listeningIntervalMS);
+            //LOG.info("Starting server at port = " + port);
+
+            while (!stop) {
+                try {
+                    Socket clientSocket = serverSocket.accept();
+                    //LOG.info("Client accepted: " + clientSocket.toString());
+
+                    // This thread will handle the new Client
+                    new Thread(() -> {
+                        handleClient(clientSocket);
+                    }).start();
+
+                } catch (SocketTimeoutException e){
+                    //LOG.debug("Socket timeout");
+                }
+            }
+        } catch (IOException e) {
+            //LOG.error("IOException", e);
+        }
+    }
+
+    private void handleClient(Socket clientSocket) {
+        try {
+            strategy.serverStrategy(clientSocket.getInputStream(), clientSocket.getOutputStream());
+            //LOG.info("Done handling client: " + clientSocket.toString());
+            clientSocket.close();
+        } catch (IOException e){
+            //LOG.error("IOException", e);
+        }
+    }
+
+    public void stop(){
+        //LOG.info("Stopping server...");
+        stop = true;
+    }
+
+    @Override
+    public void serverStrategy(InputStream inFromServer, OutputStream outToServer) {
+
+    }
+}
