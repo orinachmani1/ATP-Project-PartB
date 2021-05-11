4df4c0cc096510708f98674b2e25d788a47aefc9
Shaharasa <shaharasg1@gmail.com>
2021-05-11 18:39:36 +0300

    COMMIT TO GET THE PULL

--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -1,11 +1,96 @@
 package Server;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import algorithms.mazeGenerators.Maze;
+import algorithms.search.*;
+
+import java.io.*;
+import static Server.Configurations.properties;
 
 public class ServerStrategySolveSearchProblem implements IServerStrategy{
     @Override
-    public void serverStrategy(InputStream inFromClient, OutputStream outToClient) {
+    public void serverStrategy(InputStream inFromClient, OutputStream outToClient) throws IOException, ClassNotFoundException {
+        String tempDirectoryPath = System.getProperty("java.io.tmpdir");
+        ObjectInputStream fc = new ObjectInputStream((inFromClient));
+        ObjectOutputStream tc = new ObjectOutputStream((outToClient));
+        tc.flush();
+
+        Maze getMaze= (Maze)fc.readObject();
+        int hashC= getMaze.toString().hashCode();
+        String tempPath = tempDirectoryPath+hashC;
+        File file = new File(tempPath);
+
+        if(file.exists())
+        {
+//            String nameOfAlgo = properties.getProperty("SearchAlgo");
+//            String solvingAlgorithm = "DFS";
+//            String tempath=  tempDirectoryPath+hashC;//W
+//            ISearchingAlgorithm searchAlgo;
+//            if (nameOfAlgo=="DFS");
+//            {
+//                 searchAlgo = new DepthFirstSearch();
+//
+//            }
+//             if  (nameOfAlgo=="BFS"){
+//                   searchAlgo = new BreadthFirstSearch();
+//
+//
+//             }
+//            if  (nameOfAlgo=="BestFirstSearch"){
+//                  searchAlgo = new BestFirstSearch();
+//
+//
+//            }
+
+            //ISearchable searchableM= new SearchableMaze(getMaze);
+            //Solution solution = searchAlgo.solve(searchableM);
+            //tc.writeObject(solution);//2client
+            //tc.flush();
+            FileInputStream solContent= new FileInputStream(tempPath);
+            ObjectInputStream inputSol=new ObjectInputStream(solContent);
+            Solution solution;
+            solution=(Solution) inputSol.readObject();
+            tc.writeObject(solution);
+            solContent.close();
+//            FileOutputStream tempF = new FileOutputStream(tempPath);
+//            ObjectOutputStream writeM = new ObjectOutputStream(tempF);
+//            writeM.writeObject(getMaze);
+//            String soluPath = tempDirectoryPath +  + getMaze.toString().hashCode() +"solution";
+//            FileOutputStream fs = new FileOutputStream(soluPath);
+//            ObjectOutputStream writeSolu = new ObjectOutputStream(fs);
+//            writeSolu.writeObject(solution);
+
+        }
+
+
+        else{
+            String nameOfAlgo = properties.getProperty("SearchingAlgorithm");
+            ISearchingAlgorithm searchAlgo;
+            if (nameOfAlgo=="DeptFirstSearch");
+            {
+                searchAlgo = new DepthFirstSearch();
+
+            }
+            if(nameOfAlgo=="BreadthFirstSearch"){
+                searchAlgo = new BreadthFirstSearch();
+
+            }
+            if (nameOfAlgo=="BestFirstSearch"){
+                searchAlgo = new BestFirstSearch();
+            }
+
+
+            SearchableMaze sm = new SearchableMaze(getMaze);
+            Solution solution;
+            solution=searchAlgo.solve(sm);
+            FileOutputStream fileOutputStream = new FileOutputStream(tempPath);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream((fileOutputStream));
+            objectOutputStream.flush();
+            objectOutputStream.writeObject(solution);
+            objectOutputStream.flush();
+            fileOutputStream.close();
+            objectOutputStream.close();
+            tc.writeObject(solution);
+        }
 
     }
 }
--- src/test/RunCompressDecompressMaze.java
+++ src/test/RunCompressDecompressMaze.java
@@ -1,6 +1,8 @@
 package test;
 import IO.MyCompressorOutputStream;
 import IO.MyDecompressorInputStream;
+import IO.SimpleCompressorOutputStream;
+import IO.SimpleDecompressorInputStream;
 import algorithms.mazeGenerators.AMazeGenerator;
 import algorithms.mazeGenerators.Maze;
 import algorithms.mazeGenerators.MyMazeGenerator;
@@ -11,11 +13,11 @@ public class RunCompressDecompressMaze {
     public static void main(String[] args) {
         String mazeFileName = "savedMaze.maze";
         AMazeGenerator mazeGenerator = new MyMazeGenerator();
-        Maze maze = mazeGenerator.generate(999, 999); //Generate new maze
+        Maze maze = mazeGenerator.generate(10, 10); //Generate new maze
         maze.print();//TODO - REMOVE
         try {
 // save maze to a file
-            OutputStream out = new MyCompressorOutputStream(new FileOutputStream(mazeFileName));
+            OutputStream out = new SimpleCompressorOutputStream(new FileOutputStream(mazeFileName));
             out.write(maze.toByteArray());
             out.flush();
             out.close();
@@ -25,7 +27,7 @@ public class RunCompressDecompressMaze {
         byte savedMazeBytes[] = new byte[0];
         try {
 //read maze from file
-            InputStream in = new MyDecompressorInputStream(new FileInputStream(mazeFileName));
+            InputStream in = new SimpleDecompressorInputStream(new FileInputStream(mazeFileName));
             savedMazeBytes = new byte[maze.toByteArray().length];
             in.read(savedMazeBytes);//output deco[12,0,102,2]
             in.close();
